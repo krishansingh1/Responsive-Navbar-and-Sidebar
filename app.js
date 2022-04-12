15fae9f3fd101fa9d2d551796014f5c2e460674b
krishansingh1 <kkrishan7074@gmail.com>
2022-04-12 14:27:20 +0530

    Commit

--- app.js
+++ app.js
@@ -3,13 +3,5 @@ const links = document.querySelector(".links");
 
 
 navToggle.addEventListener("click", () => {
-    // console.log(links.classList);
-    // console.log(links.classList.contains("random"));
-    // console.log(links.classList.contains("links"));
-    // if (links.classList.contains("show-links")) {
-    //     links.classList.remove("show-links");
-    // } else {
-    //     links.classList.add("show-links");
-    // }
     links.classList.toggle("show-links");
 });
