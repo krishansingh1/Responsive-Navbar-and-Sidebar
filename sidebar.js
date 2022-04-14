f7cadeb8f2565a8726b9562e2620d9ac3bb5e235
krishansingh1 <kkrishan7074@gmail.com>
2022-04-14 12:08:00 +0530

    Commit

---
+++ sidebar.js
@@ -0,0 +1,12 @@
+const sidebar_open = document.querySelector('.sidebar-toggle');
+const sidebar_close = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar')
+
+sidebar_open.addEventListener('click', () => {
+    //console.log(sidebar.classList.toggle);
+    sidebar.classList.toggle('show-sidebar');
+})
+
+sidebar_close.addEventListener('click', () => {
+    sidebar.classList.remove('show-sidebar')
+})
